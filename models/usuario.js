acf52c695281774209f21a39c34e2a884678ccac
margaritasing <libiamfreitesm@gmail.com>
2023-04-07 18:03:23 -0300

    subo el modelo de usuarios

---
+++ models/usuario.js
@@ -0,0 +1,37 @@
+const { Schema, model }  = require('mongoose');
+
+const UsuariosSchema = Schema({
+    nombre:{
+        type:String,
+        required:[true, "El nombre es obligado"]
+    }, 
+    correo:{
+        type:String,
+        required:[true, "El correo es obligado"],
+        unique:true
+    },
+    password:{
+        type:String,
+        required:[true, "La contraseña es obligatoria"]        
+    },
+    img:{
+        type:String             
+    },
+    rol:{
+        type:String,
+        required:true,
+        emun:['ADMIN_ROLE', 'USER_ROLE']             
+    },
+    estado:{
+        type:Boolean,
+        default:true
+    },
+    google:{
+        type:Boolean,
+        default:true
+    }
+});
+
+
+
+module.exports = model( 'Usuario', UsuariosSchema );
